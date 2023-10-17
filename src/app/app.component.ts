51baa7a213258962dd84c4d5e2de8fb11087cec8
Nishant Krishna Ghadigaonkar <nishantkrishna1288@gmail.com>
2023-10-17 23:29:54 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GetApiService } from './get-api.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-angular-project';
+  constructor(private api: GetApiService) {
+
+  }
+  // ngOnInit() {
+  //   this.api.apiCall().subscribe((data) => {
+  //     console.warn("get api data", data);
+  //     this.title = data['title']
+  //   })
+  // }
+
+  newFunc() {
+    console.log('click');
+    this.api.apiCall().subscribe((data) => {
+      // console.warn(data['data'][0]['country']);
+      // this.title = data['title']
+      let countryName = data['data']
+      countryName.map((value:any) => {
+        console.log(value.country)
+      } )
+    })
+  }
 }
